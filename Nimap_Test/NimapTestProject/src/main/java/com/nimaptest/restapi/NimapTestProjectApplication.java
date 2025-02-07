1618cbde42d6a01dd758311c7c1f8a62e1b9c5a4
Rohit Suryavanshi <158577988+Suryavanshirohit@users.noreply.github.com>
2025-02-07 23:11:52 +0530

    Add files via upload

---
+++ Nimap_Test/NimapTestProject/src/main/java/com/nimaptest/restapi/NimapTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nimaptest.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimapTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimapTestProjectApplication.class, args);
+	}
+
+}
